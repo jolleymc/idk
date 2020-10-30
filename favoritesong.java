668fcf9a562577ebc9197bbba526ad6a2172f5dc
Michael Jolley <73536949+jolleymc@users.noreply.github.com>
2020-10-30 13:27:06 -0400

    Add files via upload

---
+++ favoritesong.java
@@ -0,0 +1,42 @@
+/*Favorite Sng Java Homework
+This is my favorite song Java homework.
+This file consists of strings and characters that pertain to a song I enjoy listening to.
+This file was created by Michael Jolley*/
+
+package FavoriteSongHW;
+
+public class favoritesong {
+    
+    public static void main(String[] args) {
+        String c = "Hip Hop";
+        String s1 = "Song Title: Middle Child, ";
+        String s2 = "Written by J. Cole, ";
+        String s3 = "Has been No. 4 on the Hot 100 Chart, ";
+        String s4 = (s1 + s2 + s3);
+        String s5 = "January 26, 2019";
+        
+        char character1 = 'R';
+        char character2 = 'e';
+        char character3 = 'l';
+        char character4 = 'e';
+        char character5 = 'a';
+        char character6 = 's';
+        char character7 = 'e';
+        char character8 = 'd';
+        
+    
+    
+       
+        System.out.println(s4);
+        System.out.println(character1);
+        System.out.println(character2);
+        System.out.println(character3);
+        System.out.println(character4);
+        System.out.println(character5);
+        System.out.println(character6);
+        System.out.println(character7);
+        System.out.println(character8);
+        System.out.println(s5);
+        
+    }
+}
